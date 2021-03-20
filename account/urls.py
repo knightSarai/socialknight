f20edd377231e3cae150794fd91ea69b0ce63b2f
knight <knight.sarai.dev@gmail.com>
2021-03-20 18:03:36 +0200

    Init project Add bootstrap Add login/logout functionality Add Change password  functionality

---
+++ account/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from django.contrib.auth import views as auth
+from .views import dashboard
+
+urlpatterns = [
+    path('login/', auth.LoginView.as_view(), name='login'),
+    path('logout/', auth.LogoutView.as_view(), name='logout'),
+    path('password_change/', auth.PasswordChangeView.as_view(), name='password_change'),
+    path('password_change/done/', auth.PasswordChangeDoneView.as_view(), name='password_change_done'),
+    path('', dashboard, name='dashboard'),
+]
+
