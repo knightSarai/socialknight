546cbcbbc665ea03ada2cb2c26bb0e5d8cc4c38c
knight <knight.sarai.dev@gmail.com>
2021-04-12 10:27:58 +0300

    Add Ajax to like/unlike an image

--- images/views.py
+++ images/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render, redirect, get_object_or_404
-from django.contrib.auth.decorators import login_required
+import json
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.http import JsonResponse
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views.decorators.http import require_POST
+
 from .forms import ImageCreateForm
 from .models import Image
 
@@ -28,6 +32,7 @@ def image_create(request):
         }
     )
 
+
 def image_detail(request, id, slug):
     image = get_object_or_404(Image, id=id, slug=slug)
 
@@ -35,4 +40,24 @@ def image_detail(request, id, slug):
         'section': 'images',
         'image': image
     }
-    return render(request, 'images/image/detail.html',context)
+    return render(request, 'images/image/detail.html', context)
+
+
+@login_required
+@require_POST
+def image_like(request):
+    json_data = json.loads(request.body)
+    image_id = json_data.get('id')
+    action = json_data.get('action')
+    if image_id and action:
+        try:
+            image = Image.objects.get(id=image_id)
+            if action == 'like':
+                image.users_like.add(request.user)
+            else:
+                image.users_like.remove(request.user)
+            return JsonResponse({'status': 'ok'})
+        except Exception as e:
+            pass
+
+    return JsonResponse({'status': 'error'})
